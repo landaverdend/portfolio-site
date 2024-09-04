25f3004e96e4978f377528aa3e44efa4a8b6ec95
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-09-04 19:41:41 -0300

    cleaned up some styling

--- src/components/cardStack/CardStack.tsx
+++ src/components/cardStack/CardStack.tsx
@@ -11,36 +11,38 @@ interface CardProps {
   // isActive: boolean;
 }
 function Card(props: CardProps) {
+  const { isFirst } = props;
   const [isActive, setIsActive] = useState<Boolean>(false);
 
   const handleMouseEnter = () => {
-    console.log('Mouse entered the element ' + props.hoverText);
+    setIsActive(true);
   };
 
   const handleMouseLeave = () => {
-    console.log('Mouse left the element.');
+    setIsActive(false);
   };
 
   return (
-    <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <img src={props.src} />
-    </div>
+    <img
+      className={'card--image ' + (isFirst ? 'card--active' : '')}
+      src={props.src}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    />
   );
 }
 
 function CardStack() {
   // This could also just be a prop
   const cards = [
-    <Card key={blurredResume} src={blurredResume} hoverText={'lol'} isFirst={true}/>,
-    <Card key={coverLetterBlurred} src={coverLetterBlurred} hoverText={'lol'} isFirst={false}/>,
+    <Card key={blurredResume} src={blurredResume} hoverText={'lol'} isFirst={true} />,
+    <Card key={coverLetterBlurred} src={coverLetterBlurred} hoverText={'lol'} isFirst={false} />,
     <Card key={gigachadBlurred} src={gigachadBlurred} hoverText={'lol'} isFirst={false} />,
   ];
 
   return (
     <div>
-      <div className="card-stack">{
-      cards
-      }</div>
+      <div className="card-stack">{cards}</div>
     </div>
   );
 }
