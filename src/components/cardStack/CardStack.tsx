a406f039e65aaca0c0dc81d10ca247c394eea6a3
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-09-04 13:33:52 -0300

    card stack...

--- src/components/cardStack/CardStack.tsx
+++ src/components/cardStack/CardStack.tsx
@@ -1,24 +1,47 @@
-import blurredResume from '@assets/images/blurred-resume.png';
+import blurredResume from '@assets/images/blurredResume.png';
+import coverLetterBlurred from '@assets/images/coverLetterBlurred.png';
+import gigachadBlurred from '@assets/images/gigachadBlurred.png';
 import './card-stack.css';
+import { useState } from 'react';
 
 interface CardProps {
   src: string;
+  hoverText: string;
+  isFirst: boolean;
+  // isActive: boolean;
 }
 function Card(props: CardProps) {
+  const [isActive, setIsActive] = useState<Boolean>(false);
+
+  const handleMouseEnter = () => {
+    console.log('Mouse entered the element ' + props.hoverText);
+  };
+
+  const handleMouseLeave = () => {
+    console.log('Mouse left the element.');
+  };
+
   return (
-    <>
-      <img className="card" src={props.src} />
-    </>
+    <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <img src={props.src} />
+    </div>
   );
 }
 
 function CardStack() {
-  const cards = [<Card key={blurredResume} src={blurredResume} />];
+  // This could also just be a prop
+  const cards = [
+    <Card key={blurredResume} src={blurredResume} hoverText={'lol'} isFirst={true}/>,
+    <Card key={coverLetterBlurred} src={coverLetterBlurred} hoverText={'lol'} isFirst={false}/>,
+    <Card key={gigachadBlurred} src={gigachadBlurred} hoverText={'lol'} isFirst={false} />,
+  ];
 
   return (
-    <>
-      <div className="card-array">{cards}</div>
-    </>
+    <div>
+      <div className="card-stack">{
+      cards
+      }</div>
+    </div>
   );
 }
 
