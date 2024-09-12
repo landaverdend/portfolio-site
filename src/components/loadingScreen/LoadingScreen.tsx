5fb6c23860dd306210865fc9461d3b6614290b64
Nicodemus  Landaverde <nlandaverde@Nicodemuss-Air.lan>
2024-09-11 21:33:05 -0300

    uhhh

--- src/components/loadingScreen/LoadingScreen.tsx
+++ src/components/loadingScreen/LoadingScreen.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useState } from 'react';
 import './loading-screen.css';
+import logo from '@assets/images/logo.png';
 
 interface LoadingScreenProps {
   children: ReactNode;
@@ -8,14 +9,30 @@ interface LoadingScreenProps {
 
 function LoadingScreen(props: LoadingScreenProps) {
   const { children, isLoading } = props;
+  const [curtainAnimationDone, setCurtainAnimationDone] = useState(false);
 
   return (
     <>
       <div className={`curtain-container ${isLoading ? 'curtain-container--active' : ''}`}>
-        <div className="curtain curtain--left"></div>
-        <div className="curtain curtain--right"></div>
+        <div
+          className="curtain curtain--left"
+          onAnimationEnd={() => {
+            setCurtainAnimationDone(true);
+          }}></div>
+        <div
+          className="curtain curtain--right"
+          onAnimationEnd={() => {
+            setCurtainAnimationDone(true);
+          }}></div>
       </div>
-      <div>{children}</div>
+
+      {!curtainAnimationDone ? (
+        <div>{children}</div> // render the children
+      ) : (
+        <div className="curtain-content">
+          <img className="curtain-content__image" src={logo} />
+        </div>
+      )}
     </>
   );
 }
