70cc4eee2eb5e8f7b1d075434cb2428545809fe9
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-09-15 15:55:28 -0300

    i am the funniest man in the universe.

--- src/components/loadingBar/LoadingBar.tsx
+++ src/components/loadingBar/LoadingBar.tsx
@@ -1,9 +1,27 @@
 import { useEffect, useState } from 'react';
 import './loading-bar.css';
 
+const fakeServiceCalls = [
+  'Connecting to the ByteBuster Bitter Botter API...',
+  'Spinning up instance of Gizzer Ginker Load Drinker...',
+  'Securing your packets using the Gitmo Garbler...',
+  'Whirling up the GiggleSync Chirpy-Tweet Server...',
+  'Balancing loads with the FiddleFaddle Load-Juggler...',
+  'Routing through the WiggleWonk Proxy Shuffler...',
+  'Deploying with the Puff-a-Lump Docker Flocker...',
+  'Tuning the TiddleTock WebSocket Chirper-Pinger...',
+  'Toggling flags in the Flip-Flop Feature-Switch Clicker...',
+  'Balancing loads with the WobbleWomp Elastic Whirler...',
+  'Listening in on the SqueakyQueue Message Catcher...',
+  'Sniffing logs with the CloudWatch Snicker-Snooper Monitor...',
+  'Encrypting secrets with the WobbleVault KMS Lock-Clicker...',
+  'Autoscaling with the Grow-a-Lot EC2 Popper Upper...',
+];
+
 // TODO: style this for mozilla.
 function LoadingBar() {
   const [progress, setProgress] = useState(15);
+  const [ind, setInd] = useState(0);
 
   useEffect(() => {
     const tickProgress = setInterval(() => {
@@ -17,6 +35,10 @@ function LoadingBar() {
       });
     }, 100);
 
+    const setServiceCall = setInterval(() => {
+      setInd(Math.floor(Math.random() * fakeServiceCalls.length));
+    }, 3000);
+
     return () => {
       clearInterval(tickProgress);
     };
@@ -24,7 +46,7 @@ function LoadingBar() {
 
   return (
     <div className="loading-container">
-      <span className="loading-container__label">hello</span>
+      <span className="loading-container__label">{fakeServiceCalls[ind]}</span>
       <progress className="loading-container__progress" max={100} value={progress}></progress>
     </div>
   );
