12eec5456a333b4f255286b91b84be42b4ca82f4
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-09-28 16:19:43 -0300

    hmm

--- src/views/resumeView/ResumeView.tsx
+++ src/views/resumeView/ResumeView.tsx
@@ -1,11 +1,30 @@
 import Navbar from '@/components/navbar/Navbar';
 import './resume-view.css';
-import me from '@assets/images/me.jpeg';
+import me from '@assets/images/me.png';
+import voltorb from '@assets/images/trimmedVoltorb.gif';
+
+type PCProps = {
+  src: string;
+  desc: string;
+};
+function ProjectCard({ src, desc }: PCProps) {
+  return (
+    <div className="project-card">
+      <img src={src} height={500} width={700} />
+      <p>{desc}</p>
+    </div>
+  );
+}
 
 function ProjectContainer() {
   return (
     <div id="projects" className="projects-container">
-      projects
+      <ProjectCard
+        src={voltorb}
+        desc={
+          'ReactJS recreation of the Voltorb Flip minigame from Pokemon HeartGold/SoulSilver with some slight difficulty alterations.'
+        }
+      />
     </div>
   );
 }
@@ -21,7 +40,12 @@ function PricingContainer() {
 function AboutContainer() {
   return (
     <div id="about" className="about-container">
-      about
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+      </p>
       <img src={me} />
     </div>
   );
@@ -32,8 +56,8 @@ function ResumeView() {
     <div className="resume-container">
       <Navbar />
       <AboutContainer />
-      <PricingContainer />
       <ProjectContainer />
+      <PricingContainer />
     </div>
   );
 }
