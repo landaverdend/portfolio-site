986234a9ab8f0a00befa3ea7cbd9e4624eb5850f
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-10-09 20:25:48 -0300

    why do i need a fucking message every time I change something

--- src/views/resumeView/footer/Footer.tsx
+++ src/views/resumeView/footer/Footer.tsx
@@ -9,7 +9,9 @@ function Footer() {
     <footer className="footer-container">
       <div className="footer-container__info">
         <div className="footer-container__logo">
-          <img src={siteLogo} height={75} width={75} />
+          <a href="https://github.com/landaverdend">
+            <img src={siteLogo} height={75} width={75} />
+          </a>
           <span>© Fields Of Green Studios 2024 </span>
         </div>
         <div className="footer-container__links">
