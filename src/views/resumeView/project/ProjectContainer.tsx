c8b4af6aa7b5029f5793d6b38d5b6dce695322a8
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-10-10 20:59:57 -0300

    updated styling again.

--- src/views/resumeView/project/ProjectContainer.tsx
+++ src/views/resumeView/project/ProjectContainer.tsx
@@ -8,18 +8,23 @@ type PCProps = {
   src: string;
   desc: string;
   techUsed: string;
+  title: string;
 };
-function ProjectCard({ src, desc }: PCProps) {
+function ProjectCard({ src, desc, techUsed, title }: PCProps) {
   return (
     <div className="project-card">
       <div className="project-card__img">
         <img src={src} height={200} width={300} />
       </div>
 
-      <div className="project-card__text-container">
-        <p>{desc}</p>
+      <div className="text-container">
+        <h3>{title}</h3>
+
+        <p className="text-container__tech-used">
+          Technologies used: <span className="courier-text">{techUsed}</span>
+        </p>
+        <p className="text-container__description">{desc}</p>
       </div>
-    
     </div>
   );
 }
@@ -28,20 +33,23 @@ function ProjectContainer() {
   return (
     <div id="projects" className="projects-container">
       <ProjectCard
+        title={'Voltorb Flip JS'}
         src={voltorb}
-        techUsed={''}
+        techUsed={'React, Redux, NodeJS'}
         desc={
           'ReactJS recreation of the Voltorb Flip minigame from Pokemon HeartGold/SoulSilver with some slight difficulty alterations.'
         }
       />
       <ProjectCard
+        title={'Isle Advance'}
         src={isleAdvanceGIF}
-        techUsed={''}
+        techUsed={'C, Make'}
         desc={'Gameboy Advance Homebrew project made alongside my friend for our Senior Year capstone project'}
       />
       <ProjectCard
+        title={'Tetris Galaxy'}
         src={tetrisGalaxyGif}
-        techUsed={''}
+        techUsed={'Java, Java Swing'}
         desc={'Tetris spinoff that was made as a hackathon project over one very long night alongside my 3 friends'}
       />
     </div>
