ba138287236491f385616cd5b1c5c9c4c7602fa3
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-10-12 13:43:39 -0300

    added more bubbles like a ******

--- src/views/resumeView/ResumeView.tsx
+++ src/views/resumeView/ResumeView.tsx
@@ -13,6 +13,8 @@ function ResumeView() {
         <AboutContainer />
         <div className="bubble b1"></div>
         <div className="bubble b2"></div>
+        <div className="bubble b3"></div>
+        <div className="bubble b4"></div>
         <ProjectContainer />
       </div>
       <Footer />
