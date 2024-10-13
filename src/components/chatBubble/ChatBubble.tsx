832df844bc1f658e6d49eed94368e0ab338e777b
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-10-12 23:41:48 -0300

    updated the chat bubble styling...

--- src/components/chatBubble/ChatBubble.tsx
+++ src/components/chatBubble/ChatBubble.tsx
@@ -1,10 +1,54 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './chatBubble.css';
 import me from '@assets/images/meBlob.png';
 
+type Chat = {
+  sender: 'me' | 'you';
+  content: string;
+};
+
+type MCProps = {
+  closeFn: Function;
+  chatLog: Array<Chat>;
+};
+function MessageContainer({ chatLog, closeFn }: MCProps) {
+  return (
+    <div className="messenger-container">
+      <div className="messenger-container__header">
+        <img src={me} height={25} width={25} />
+        <span>Nicopenis Landaverdgay</span>
+        <span className="messenger-container__close" onClick={() => closeFn()}>
+          &#10005;
+        </span>
+      </div>
+      <div className="messenger-container__messages">
+        {chatLog.map((chat) => {
+          return (
+            <div className={chat.sender === 'me' ? 'sender--ME' : 'sender--YOU'}>
+              <span className={chat.sender === 'me' ? 'chat-text ME' : 'chat-text YOU'}>{chat.content}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="messenger-container__sendField">this here is the chat log.</div>
+    </div>
+  );
+}
+
 function ChatBubble() {
   const [isOpen, setIsOpen] = useState(false);
+
   const [unreadMessages, setUnreadMessages] = useState(3);
+  const [chatLog, setChatLog] = useState<Array<Chat>>([]);
+
+  useEffect(() => {
+    const toPut: Chat[] = [];
+    for (let i = 0; i < 25; i++) {
+      toPut.push({ sender: 'me', content: 'message number ' + i });
+      toPut.push({ sender: 'you', content: 'message number ' + i });
+    }
+    setChatLog(toPut);
+  }, []);
 
   return (
     <>
@@ -22,22 +66,8 @@ function ChatBubble() {
           }
         </div>
       )}
-      {/* CHAT CONTAINER Code...*/}
-      {isOpen && (
-        <div className="messenger-container">
-          <div className="messenger-container__header">
-            <img src={me} height={25} width={25} />
-            <span>Nicopenis Landaverdgay</span>
-            <span
-              className="messenger-container__close"
-              onClick={() => {
-                setIsOpen((prev) => !prev);
-              }}>
-              &#10005;
-            </span>
-          </div>
-        </div>
-      )}
+
+      {isOpen && <MessageContainer closeFn={setIsOpen} chatLog={chatLog} />}
     </>
   );
 }
