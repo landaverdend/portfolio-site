937bf45c516d139c5369b102a6d19b686809cf21
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-10-12 22:16:36 -0300

    adding chat bubble component...

--- src/components/chatBubble/ChatBubble.tsx
+++ src/components/chatBubble/ChatBubble.tsx
@@ -3,15 +3,42 @@ import './chatBubble.css';
 import me from '@assets/images/meBlob.png';
 
 function ChatBubble() {
-  const [unreadMessages, setUnreadMessages] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
+  const [unreadMessages, setUnreadMessages] = useState(3);
 
   return (
-    <div className="chat-container">
-      <div className="chat-container__me">
-        {unreadMessages > 0 ? <span className="chat-container__unread">{unreadMessages}</span> : <></>}
-        <img src={me} height={60} width={60} />
-      </div>
-    </div>
+    <>
+      {!isOpen && (
+        <div className="chat-container">
+          {
+            <div
+              className="chat-container__me"
+              onClick={() => {
+                setIsOpen((prev) => !prev);
+              }}>
+              {unreadMessages > 0 ? <span className="chat-container__unread">{unreadMessages}</span> : <></>}
+              <img src={me} height={60} width={60} />
+            </div>
+          }
+        </div>
+      )}
+      {/* CHAT CONTAINER Code...*/}
+      {isOpen && (
+        <div className="messenger-container">
+          <div className="messenger-container__header">
+            <img src={me} height={25} width={25} />
+            <span>Nicopenis Landaverdgay</span>
+            <span
+              className="messenger-container__close"
+              onClick={() => {
+                setIsOpen((prev) => !prev);
+              }}>
+              &#10005;
+            </span>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
