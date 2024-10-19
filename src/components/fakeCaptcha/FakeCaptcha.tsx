c4e2c770a43e042d08cbe751fab6cb7d82c025cc
Nicodemus  Landaverde <nlandaverde@Nicodemuss-Air.lan>
2024-10-19 20:56:10 -0300

    added captcha lookalike..

--- src/components/fakeCaptcha/FakeCaptcha.tsx
+++ src/components/fakeCaptcha/FakeCaptcha.tsx
@@ -1,11 +1,19 @@
 import './fake-captcha.css';
+import captchaIcon from '@assets/images/icons/captcha-icon.png';
 
 function FakeCaptcha() {
   return (
     <div className="captcha-container">
-      <span className="captcha-container__checkbox">checkbox</span>
-      <span className="captcha-container__text">I'm not a robot</span>
-      <span className="captcha-container__about">stuff</span>
+      <div className="captcha-container__left">
+        <span className="captcha-container__checkbox"></span>
+        <span className="captcha-container__text">I'm not a robot</span>
+      </div>
+
+      <span className="captcha-container__about">
+        <img src={captchaIcon} height={30} width={30}></img>
+        <span style={{ fontSize: '0.85rem' }}>reCAPTCHA</span>
+        <span style={{ fontSize: '0.75rem' }}>Privacy - Terms</span>
+      </span>
     </div>
   );
 }
