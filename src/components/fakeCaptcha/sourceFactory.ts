62e1d5babe265adb9dc601390164f4e3f91dd64d
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-10-27 19:25:26 -0300

    did some funnies.

--- src/components/fakeCaptcha/sourceFactory.ts
+++ src/components/fakeCaptcha/sourceFactory.ts
@@ -1,14 +1,23 @@
 import OJ from '@assets/images/captcha/bigGrid/OJ.png';
+import china from '@assets/images/captcha/bigGrid/china.png';
+import argentina from '@assets/images/captcha/bigGrid/argentina.png';
+import waldo from '@assets/images/captcha/bigGrid/waldo.png';
 
 export type LargeCaptchaChallenge = {
   imageSrc: string;
   title: string;
 };
 
-const largeCaptchaArray: Array<LargeCaptchaChallenge> = [{ imageSrc: OJ, title: 'Guilty Murderers' }];
+const largeCaptchaArray: Array<LargeCaptchaChallenge> = [
+  { imageSrc: OJ, title: 'Guilty Murderers' },
+  { imageSrc: china, title: 'Chinese Sovereign Territory' },
+  { imageSrc: argentina, title: 'Argentinian Territorial Domain' },
+  { imageSrc: waldo, title: 'Waldo' },
+];
 
 export function grabRandomChallengeLarge() {
   const ind = Math.floor(Math.random() * largeCaptchaArray.length);
 
   return largeCaptchaArray[ind];
+  // return largeCaptchaArray[3];
 }
