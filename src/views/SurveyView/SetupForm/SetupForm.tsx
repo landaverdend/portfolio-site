b24b9c6b46f48c82a65483d3afa6789c63975685
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-11-15 00:03:43 -0300

    rain drops are fallin.

--- src/views/SurveyView/SetupForm/SetupForm.tsx
+++ src/views/SurveyView/SetupForm/SetupForm.tsx
@@ -3,6 +3,8 @@ import { useEffect, useRef, useState } from 'react';
 import { FieldError, SubmitHandler, useForm } from 'react-hook-form';
 
 import '../survey-view.css';
+import { createPortal } from 'react-dom';
+import { Bodies, Composite, Engine, Runner } from 'matter-js';
 
 function randomChanceInTen(num: number) {
   return Math.random() < num * 0.1;
@@ -39,152 +41,253 @@ function SetupForm() {
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {};
 
+  // Build the Matter.JS stuff
+  const ref = useRef<HTMLDivElement>(null);
+  const engine = useRef<Matter.Engine>(Engine.create());
+  const runner = useRef<Matter.Runner>(Runner.create());
+  const [, setAnim] = useState(0);
+  const dots = useRef<{ x: number; y: number }[]>([]);
+
+  useEffect(() => {
+    Runner.run(runner.current, engine.current);
+  }, []);
+
+  // Build out the base scene when times tried is greater or equal to 1.
+  useEffect(function init() {
+    const width = ref.current?.clientWidth ?? 0;
+    const height = ref.current?.clientHeight ?? 0;
+
+    const ground = Bodies.rectangle(width / 2, height, width, 50, { isStatic: true });
+    const ceiling = Bodies.rectangle(width / 2, 0, width, 1, {
+      isStatic: true,
+    });
+    const wallL = Bodies.rectangle(0, height / 2, 1, height, {
+      isStatic: true,
+    });
+    const wallR = Bodies.rectangle(width, height / 2, 50, height, {
+      isStatic: true,
+    });
+
+    Composite.add(engine.current.world, [ground, ceiling, wallL, wallR]);
+  }, []);
+
+  useEffect(() => {
+    let unsubscribe: any;
+
+    function addElement() {
+      const width = ref.current?.clientWidth ?? 0;
+      const height = ref.current?.clientHeight ?? 0;
+      const circ = Bodies.circle(Math.random() * width * 0.75 + 50, Math.random() * height * 0.75 + 50, 25);
+      circ.friction = 0.05;
+      circ.frictionAir = 0.00005;
+      circ.restitution = 0.9;
+
+      Composite.add(engine.current.world, circ);
+
+      if (dots.current.length < 100) setTimeout(addElement, 300);
+    }
+
+    addElement();
+
+    return () => {
+      clearTimeout(unsubscribe);
+    };
+  }, []);
+
+  useEffect(function triggerAnimation() {
+    let unsubscribe: number;
+    function animate() {
+      let i = 0;
+      for (const el of Composite.allBodies(engine.current.world)) {
+        if (el.isStatic) continue;
+
+        dots.current[i] = { x: el.position.x, y: el.position.y };
+
+        i++;
+      }
+      setAnim((x) => x + 1);
+      unsubscribe = requestAnimationFrame(animate);
+    }
+
+    unsubscribe = requestAnimationFrame(animate);
+
+    return () => {
+      cancelAnimationFrame(unsubscribe);
+    };
+  }, []);
+
   return (
-    <div className="email-form-container">
-      <h1>Let's get you started...</h1>
-      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-      <form className={'email-form'} onSubmit={handleSubmit(onSubmit)}>
-        {/* include validation with required or other standard HTML validation rules */}
+    <>
+      {createPortal(
+        <>
+          <div ref={ref} style={{ height: '100vh', width: '100vw', position: 'absolute' }}></div>
+        </>,
+        document.getElementById('root') as HTMLElement
+      )}
 
-        <label htmlFor="firstName" className="firstName">
-          First Name
-          <input
-            {...register('firstName', {
-              required: { value: true, message: 'You forgot the first name....' },
-              validate: {
-                badName: () => randomChanceInTen(3) || 'Hmmmm, pick a better name...',
-              },
-            })}
-            name="firstName"
-          />
-          {/* errors will return when field validation fails  */}
-          {errors.firstName && <ErrorText error={errors.firstName}></ErrorText>}
-        </label>
+      {dots.current.map((dot, key) => {
+        return (
+          <div
+            style={{
+              backgroundColor: 'red',
+              borderRadius: '50%',
+              position: 'absolute',
+              top: dot.y,
+              left: dot.x,
+              width: '50px',
+              height: '50px',
+              zIndex: 10000,
+            }}
+            key={key}></div>
+        );
+      })}
 
-        <label className="lastName">
-          Last Name
-          <input
-            {...register('lastName', {
-              required: { value: true, message: 'Whoa there! You need to add a last name!' },
-              validate: {
-                badName: () => randomChanceInTen(3) || "This one just won't work.... sorry!",
-              },
-            })}></input>
-          {errors.lastName && <ErrorText error={errors.lastName} />}
-        </label>
+      <div className="email-form-container">
+        <h1>Let's get you started...</h1>
+        {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+        <form className={'email-form'} onSubmit={handleSubmit(onSubmit)}>
+          {/* include validation with required or other standard HTML validation rules */}
 
-        <div className="user-details">
-          <label>
-            Work Email
+          <label htmlFor="firstName" className="firstName">
+            First Name
             <input
-              type="text"
-              placeholder="Email"
-              {...register('email', {
-                required: { value: true, message: 'Sorry but I need your email!!!' },
-                pattern: { value: /^\S+@\S+$/i, message: 'Please enter an EMAIL' },
+              {...register('firstName', {
+                required: { value: true, message: 'You forgot the first name....' },
                 validate: {
-                  badEmail: () => randomChanceInTen(2) || 'I think you just made that up... Try again',
+                  badName: () => randomChanceInTen(3) || 'Hmmmm, pick a better name...',
                 },
               })}
+              name="firstName"
             />
-            {errors.email && <ErrorText error={errors.email} />}
+            {/* errors will return when field validation fails  */}
+            {errors.firstName && <ErrorText error={errors.firstName}></ErrorText>}
           </label>
 
+          <label className="lastName">
+            Last Name
+            <input
+              {...register('lastName', {
+                required: { value: true, message: 'Whoa there! You need to add a last name!' },
+                validate: {
+                  badName: () => randomChanceInTen(3) || "This one just won't work.... sorry!",
+                },
+              })}></input>
+            {errors.lastName && <ErrorText error={errors.lastName} />}
+          </label>
+
+          <div className="user-details">
+            <label>
+              Work Email
+              <input
+                type="text"
+                placeholder="Email"
+                {...register('email', {
+                  required: { value: true, message: 'Sorry but I need your email!!!' },
+                  pattern: { value: /^\S+@\S+$/i, message: 'Please enter an EMAIL' },
+                  validate: {
+                    badEmail: () => randomChanceInTen(2) || 'I think you just made that up... Try again',
+                  },
+                })}
+              />
+              {errors.email && <ErrorText error={errors.email} />}
+            </label>
+
+            <label>
+              Job Title
+              <input
+                type="text"
+                placeholder="Job Title"
+                {...register('jobTitle', {
+                  required: { value: true, message: "C'mon don't tell me your unemployed..." },
+                  validate: {
+                    badTitle: () => randomChanceInTen(2) || "That's not a job!",
+                  },
+                })}></input>
+              {errors.jobTitle && <ErrorText error={errors.jobTitle} />}
+            </label>
+
+            <label>
+              Phone Number
+              <input
+                type="tel"
+                placeholder="(123) 456-7891"
+                {...register('phone', {
+                  required: { value: true, message: "That's not a phone number!!" },
+                  validate: { badPhone: () => randomChanceInTen(1) || "Listen. I don't think so" },
+                })}></input>
+              {errors.phone && <ErrorText error={errors.phone} />}
+            </label>
+          </div>
+
           <label>
-            Job Title
+            Company Name
             <input
               type="text"
-              placeholder="Job Title"
-              {...register('jobTitle', {
-                required: { value: true, message: "C'mon don't tell me your unemployed..." },
-                validate: {
-                  badTitle: () => randomChanceInTen(2) || "That's not a job!",
-                },
+              placeholder=""
+              {...register('companyName', {
+                required: { value: true, message: 'Please enter a value here' },
+                validate: { badCompanyName: () => randomChanceInTen(1) || 'You call that a company 🥶' },
               })}></input>
-            {errors.jobTitle && <ErrorText error={errors.jobTitle} />}
+            {errors.companyName && <ErrorText error={errors.companyName} />}
           </label>
 
           <label>
-            Phone Number
-            <input
-              type="tel"
-              placeholder="(123) 456-7891"
-              {...register('phone', {
-                required: { value: true, message: "That's not a phone number!!" },
-                validate: { badPhone: () => randomChanceInTen(1) || "Listen. I don't think so" },
-              })}></input>
-            {errors.phone && <ErrorText error={errors.phone} />}
-          </label>
-        </div>
-
-        <label>
-          Company Name
-          <input
-            type="text"
-            placeholder=""
-            {...register('companyName', {
-              required: { value: true, message: 'Please enter a value here' },
-              validate: { badCompanyName: () => randomChanceInTen(1) || 'You call that a company 🥶' },
-            })}></input>
-          {errors.companyName && <ErrorText error={errors.companyName} />}
-        </label>
-
-        <label>
-          Company Size
-          <select
-            {...register('companySize', {
-              required: false,
-              validate: { badChoice: () => randomChanceInTen(3) || 'You wish.' },
-            })}>
-            <option>1-99 employees </option>
-            <option>100-299 employees</option>
-            <option>300-1999 employees</option>
-            <option>+2000 employees</option>
-            <option>I have no company</option>
-          </select>
-          {errors.companySize && <ErrorText error={errors.companySize} />}
-        </label>
-
-        <label className="details">
-          Provide more details (optional)
-          <textarea></textarea>
-        </label>
-
-        <div className="submit-container">
-          <label>
-            <input
-              type="checkbox"
-              {...register('marketingMaterials', {
-                required: true,
-                validate: {
-                  badAnswer: () => randomChanceInTen(5) || "I don't think so.",
-                },
-              })}
-            />
-            I agree to receive marketing notifications
-            {errors.marketingMaterials && <ErrorText error={errors.marketingMaterials} />}
+            Company Size
+            <select
+              {...register('companySize', {
+                required: false,
+                validate: { badChoice: () => randomChanceInTen(3) || 'You wish.' },
+              })}>
+              <option>1-99 employees </option>
+              <option>100-299 employees</option>
+              <option>300-1999 employees</option>
+              <option>+2000 employees</option>
+              <option>I have no company</option>
+            </select>
+            {errors.companySize && <ErrorText error={errors.companySize} />}
           </label>
 
-          <input
-            id="submit-button"
-            value="Let's Go"
-            type="submit"
-            onClick={() => {
-              setTimesTried((prev) => prev + 1);
-            }}></input>
-          {timesTried >= 2 && (
-            <button
-              className="give-up-button"
+          <label className="details">
+            Provide more details (optional)
+            <textarea></textarea>
+          </label>
+
+          <div className="submit-container">
+            <label>
+              <input
+                type="checkbox"
+                {...register('marketingMaterials', {
+                  required: true,
+                  validate: {
+                    badAnswer: () => randomChanceInTen(5) || "I don't think so.",
+                  },
+                })}
+              />
+              I agree to receive marketing notifications
+              {errors.marketingMaterials && <ErrorText error={errors.marketingMaterials} />}
+            </label>
+
+            <input
+              id="submit-button"
+              value="Let's Go"
+              type="submit"
               onClick={() => {
-                setIsLoading(true);
-                setNextView('ResumeView');
-              }}>
-              I give up!
-            </button>
-          )}
-        </div>
-      </form>
-    </div>
+                setTimesTried((prev) => prev + 1);
+              }}></input>
+            {timesTried >= 2 && (
+              <button
+                className="give-up-button"
+                onClick={() => {
+                  setIsLoading(true);
+                  setNextView('ResumeView');
+                }}>
+                I give up!
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 export default SetupForm;
--- src/views/SurveyView/SurveyView.tsx
+++ src/views/SurveyView/SurveyView.tsx
@@ -5,7 +5,6 @@ import BackgroundCanvas from '@/components/backgroundCanvas/BackgroundCanvas.tsx
 import Footer from '@/components/footer/Footer';
 import ChatBubble from '@/components/chatBubble/ChatBubble';
 import SetupForm from './SetupForm/SetupForm';
-import { Bodies, Composite, Engine, Runner } from 'matter-js';
 import './survey-view.css';
 
 const dumbSlogans: string[] = [
@@ -39,105 +38,9 @@ function SurveyView() {
     };
   }, []);
 
-  // Build the Matter.JS stuff
-  const ref = useRef<HTMLDivElement>(null);
-  const engine = useRef<Matter.Engine>(Engine.create());
-  const runner = useRef<Matter.Runner>(Runner.create());
-  const [, setAnim] = useState(0);
-  const dots = useRef<{ x: number; y: number }[]>([]);
-
-  useEffect(() => {
-    Runner.run(runner.current, engine.current);
-  }, []);
-
-  // Build out the base scene when times tried is greater or equal to 1.
-  useEffect(function init() {
-    // if (timesTried >= 1) {
-    const width = ref.current?.clientWidth ?? 0;
-    const height = ref.current?.clientHeight ?? 0;
-
-    const ground = Bodies.rectangle(width / 2, height, width, 50, { isStatic: true });
-    const ceiling = Bodies.rectangle(width / 2, 0, width, 1, {
-      isStatic: true,
-    });
-    const wallL = Bodies.rectangle(0, height / 2, 1, height, {
-      isStatic: true,
-    });
-    const wallR = Bodies.rectangle(width, height / 2, 50, height, {
-      isStatic: true,
-    });
-
-    Composite.add(engine.current.world, [ground, ceiling, wallL, wallR]);
-    // }
-  }, []);
-
-  useEffect(() => {
-    let unsubscribe: any;
-
-    function addElement() {
-      const width = ref.current?.clientWidth ?? 0;
-      const height = ref.current?.clientHeight ?? 0;
-
-      const circ = Bodies.circle(Math.random() * width * 0.75 + 50, Math.random() * height * 0.75 + 50, 25);
-      circ.friction = 0.05;
-      circ.frictionAir = 0.00005;
-      circ.restitution = 0.9;
-
-      Composite.add(engine.current.world, circ);
-
-      // console.log(dots);
-      // if (dots.current.length < 100) setTimeout(addElement, 300);
-    }
-
-    addElement();
-
-    return () => {
-      clearTimeout(unsubscribe);
-    };
-  }, []);
-
-  useEffect(function triggerAnimation() {
-    let unsubscribe: number;
-    function animate() {
-      let i = 0;
-      for (const el of Composite.allBodies(engine.current.world)) {
-        if (el.isStatic) continue;
-
-        dots.current[i] = { x: el.position.x, y: el.position.y };
-
-        i++;
-      }
-      setAnim((x) => x + 1);
-      unsubscribe = requestAnimationFrame(animate);
-    }
-
-    unsubscribe = requestAnimationFrame(animate);
-
-    return () => {
-      cancelAnimationFrame(unsubscribe);
-    };
-  }, []);
-
   return (
     <>
       <div className="setup-container">
-        {dots.current.map((dot, key) => {
-          return (
-            <div
-              style={{
-                backgroundColor: 'red',
-                borderRadius: '50%',
-                position: 'absolute',
-                top: dot.y,
-                left: dot.x,
-                width: '50px',
-                height: '50px',
-                zIndex: 10000,
-              }}
-              key={key}></div>
-          );
-        })}
-
         <div className="init-block su-fade-in">
           <div className="init-block__info">
             <h1>
@@ -158,7 +61,6 @@ function SurveyView() {
         {createPortal(<ChatBubble />, document.getElementById('root') as HTMLElement)}
         {createPortal(
           <>
-            <div ref={ref} style={{ height: '100vh', width: '100vw', position: 'absolute' }}></div>
             <BackgroundCanvas flipped={true} />
           </>,
           document.getElementById('root') as HTMLElement
