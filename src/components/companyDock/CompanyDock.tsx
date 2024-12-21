d3bf51d249f169adab86241a5e531f17a7bc2e4a
Nicodemus  Landaverde <nlandaverde@Nicodemuss-Air.lan>
2024-12-21 17:34:52 -0500

    added new captcha. Added more evil companies to the drawer...

--- src/components/companyDock/CompanyDock.tsx
+++ src/components/companyDock/CompanyDock.tsx
@@ -1,37 +1,36 @@
 import './company-dock.css';
-import Nyt from '@assets/images/dock/NYT.svg';
-import Meta from '@assets/images/dock/meta.svg';
-import Google from '@assets/images/dock/google.svg';
 import Temu from '@assets/images/dock/temu.svg';
-import Github from '@assets/images/dock/github.svg';
 import Amzn from '@assets/images/dock/AMZN.svg';
-import Msft from '@assets/images/dock/MSFT.svg';
-import Forbes from '@assets/images/dock/forbes.svg';
+import Reddit from '@assets/images/dock/reddit.svg';
+import Blackrock from '@assets/images/dock/Blackrock.svg';
 import Pfizer from '@assets/images/dock/Pfizer.svg';
 import Monsanto from '@assets/images/dock/monsanto.svg';
 import Nestle from '@assets/images/dock/Nestle.svg';
 import BP from '@assets/images/dock/BP.svg';
 import Pearson from '@assets/images/dock/Pearson.svg';
+import Weinstein from '@assets/images/dock/weinsteinCompany.svg';
+import UnitedHealth from '@assets/images/dock/unitedHealthcare.svg';
+import AARP from '@assets/images/dock/AARP.svg';
 
 export default function CompanyDock() {
-  const logos = [Pfizer, Monsanto, Pearson, Nyt, Nestle, BP];
-  const logos2 = [Temu, Github, Amzn, Msft, Forbes, Meta];
+  const logos = [Pfizer, Monsanto, Pearson, UnitedHealth, Nestle, BP];
+  const logos2 = [Weinstein, Temu, Blackrock, Reddit, AARP];
 
   return (
     <div id="featured" className="featured-dock">
       <h2>AS FEATURED IN</h2>
 
       <div className="dock-list-container">
-        <ul>
+        <span>
           {logos.map((logo) => (
             <img id={crypto.randomUUID()} src={logo} className="logo" />
           ))}
-        </ul>
-        <ul>
+        </span>
+        <span>
           {logos2.map((logo) => (
             <img id={crypto.randomUUID()} src={logo} className="logo" />
           ))}
-        </ul>
+        </span>
       </div>
     </div>
   );
--- src/components/fakeCaptcha/sourceFactory.ts
+++ src/components/fakeCaptcha/sourceFactory.ts
@@ -2,9 +2,10 @@ import OJ from '@assets/images/captcha/bigGrid/OJ.png';
 import china from '@assets/images/captcha/bigGrid/china.png';
 import argentina from '@assets/images/captcha/bigGrid/argentina.png';
 import waldo from '@assets/images/captcha/bigGrid/waldo.png';
-import russia from '@assets/images/captcha/bigGrid/russia.png';
 import theDress from '@assets/images/captcha/bigGrid/thedress.jpg';
+import michaelJackson from '@assets/images/captcha/bigGrid/blackpeople.png';
 import { randomNumber } from '@/util/random';
+
 export type LargeCaptchaChallenge = {
   imageSrc: string;
   title: string;
@@ -29,16 +30,17 @@ const largeCaptchaArray: Array<LargeCaptchaChallenge> = [
   },
   { imageSrc: waldo, title: 'Waldo', solution: new Set<number>([9, 13]) },
   {
-    imageSrc: russia,
-    title: 'Russia',
-    solution: coinFlip()
-      ? new Set<number>([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-      : new Set<number>([1, 2, 3, 7, 11, 13, 14, 15]),
-  },
-  {
     imageSrc: theDress,
     title: randomNumber(0, 1) === 0 ? 'Blue Dress' : 'Gold Dress',
     solution: randomNumber(0, 1) === 0 ? new Set<number>([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]) : new Set([]),
+  },
+  {
+    imageSrc: michaelJackson,
+    title: 'Black People',
+    solution:
+      randomNumber(0, 1) === 0
+        ? new Set<number>([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        : new Set<number>([0, 1, 2, 3, 4, 5, 67, 8, 9, 12, 13]),
   },
 ];
 
@@ -47,9 +49,7 @@ export type SmallCaptchaChallenge = {
   title: string;
 };
 
-const smallCaptchaArray: Array<SmallCaptchaChallenge> = [
-  { imageTemplate: 'src/assets/images/captcha/black/b', title: 'African Americans' },
-];
+const smallCaptchaArray: Array<SmallCaptchaChallenge> = [];
 
 export function grabRandomSmallChallenge() {
   const ind = Math.floor(Math.random() * smallCaptchaArray.length);
@@ -58,8 +58,8 @@ export function grabRandomSmallChallenge() {
 }
 
 export function grabRandomLargeChallenge() {
-  const ind = Math.floor(Math.random() * largeCaptchaArray.length);
-
+  // const ind = Math.floor(Math.random() * largeCaptchaArray.length);
+  const ind = randomNumber(0, largeCaptchaArray.length);
   return largeCaptchaArray[ind];
   // return largeCaptchaArray[5];
 }
