6d4b9172293ced9fd3cd9e8e9fec1708ee457de6
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-12-21 12:35:30 -0500

    fixed dock.

--- src/components/companyDock/CompanyDock.tsx
+++ src/components/companyDock/CompanyDock.tsx
@@ -2,23 +2,23 @@ import './company-dock.css';
 import Nyt from '@assets/images/dock/NYT.svg';
 import Meta from '@assets/images/dock/meta.svg';
 import Google from '@assets/images/dock/google.svg';
-import Reddit from '@assets/images/dock/reddit.svg';
-import Salesforce from '@assets/images/dock/salesforce.svg';
-import YCombinator from '@assets/images/dock/ycombinator.svg';
 import Temu from '@assets/images/dock/temu.svg';
 import Github from '@assets/images/dock/github.svg';
 import Amzn from '@assets/images/dock/AMZN.svg';
 import Msft from '@assets/images/dock/MSFT.svg';
 import Forbes from '@assets/images/dock/forbes.svg';
+import Pfizer from '@assets/images/dock/Pfizer.svg';
 import Monsanto from '@assets/images/dock/monsanto.svg';
+import Nestle from '@assets/images/dock/Nestle.svg';
+import BP from '@assets/images/dock/BP.svg';
 
 export default function CompanyDock() {
-  const logos = [Nyt, Meta, Google, Reddit, Salesforce, YCombinator];
-  const logos2 = [Temu, Github, Amzn, Msft, Forbes, Monsanto];
+  const logos = [Pfizer, Monsanto, Google, Nyt, Nestle, BP];
+  const logos2 = [Temu, Github, Amzn, Msft, Forbes, Meta];
 
   return (
     <div id="featured" className="featured-dock">
-      <h2>AS SEEN IN</h2>
+      <h2>AS FEATURED IN</h2>
 
       <div className="dock-list-container">
         <ul>
