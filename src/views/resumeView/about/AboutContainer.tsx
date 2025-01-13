8ce94a8a7143922d767a82eb6fcab84e35ffc7df
Nicodemus  Landaverde <nlandaverde@Nicodemuss-Air.lan>
2025-01-12 20:22:25 -0500

    I did it

--- src/views/resumeView/about/AboutContainer.tsx
+++ src/views/resumeView/about/AboutContainer.tsx
@@ -5,14 +5,13 @@ function AboutContainer() {
   return (
     <>
       <div id="about" className="about-container">
-        <div className="text-container">
+        <div className="text-container__text">
           <h2>About Me</h2>
-          <div className="text-container__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-          </div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+          aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+          <div className="social-dock">SOCIALS</div>
         </div>
         <img src={me} />
       </div>
--- src/views/resumeView/skills/SkillsContainer.tsx
+++ src/views/resumeView/skills/SkillsContainer.tsx
@@ -1,5 +1,9 @@
 import './skills-container.css';
 
 export default function SkillsContainer() {
-  return <div className="skills-container">.</div>;
+  return (
+    <div className="skills-container">
+      <h1>SKILLS</h1>
+    </div>
+  );
 }
