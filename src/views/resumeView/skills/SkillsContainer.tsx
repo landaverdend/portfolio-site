f70e6087eea3b0db4b7619697ddee04559f3fb45
Nicodemus  Landaverde <nlandaverde@Nicodemuss-Air.lan>
2025-01-13 20:26:43 -0500

    shuffling some styles around. Skeleton for skills section down.

--- src/views/resumeView/skills/SkillsContainer.tsx
+++ src/views/resumeView/skills/SkillsContainer.tsx
@@ -2,32 +2,58 @@ import SubHeader from '@/components/subHeader/SubHeader';
 import './skills-container.css';
 
 type SWProps = {
-  skill: string;
-  proficiency: number;
-  logo: string;
+  techSkill: TechSkill;
 };
-function SkillWidget(props: SWProps) {
-  return <div></div>;
+function SkillWidget({ techSkill }: SWProps) {
+  const { skill, proficiency, logo } = techSkill;
+
+  return (
+    <div className="skill-widget flex-column">
+      <h3>{skill}</h3>
+      <progress className="proficiency-bar" value={proficiency} ></progress>
+    </div>
+  );
 }
 
-export default function SkillsContainer() {
-  const items = [];
+type TechSkill = {
+  skill: string;
+  proficiency: number;
+  logo?: string;
+};
 
-  for (let i = 0; i < 16; i++) {
-    items.push(
-      <span className="flex-column" style={{ backgroundColor: 'red' }}>
-        {i}
-      </span>
-    );
-  }
+const frontendSkills: Array<TechSkill> = [
+  { skill: 'React', proficiency: 84, logo: '' },
+  { skill: 'Angular', proficiency: 62, logo: '' },
+  { skill: 'CSS', proficiency: 85, logo: '' },
+  { skill: 'NodeJS', proficiency: 60, logo: '' },
+  { skill: 'Typescript/Javascript', proficiency: 89, logo: '' },
+  { skill: 'Esri API', proficiency: 56, logo: '' },
+  { skill: 'Webpack', proficiency: 65, logo: '' },
+];
+
+export default function SkillsContainer() {
+  // const items = [];
+
+  // for (let i = 0; i < 16; i++) {
+  //   items.push(
+  //     <span className="flex-column" style={{ backgroundColor: 'red' }}>
+  //       {i}
+  //     </span>
+  //   );
+  // }
 
   return (
     <div className="skills-container">
       <SubHeader>
         <h1>Skills</h1>
       </SubHeader>
-      
-      <div className="skills-grid">{items}</div>
+
+      <div className="skills-grid">
+        {/* {items} */}
+        {frontendSkills.map((skill) => (
+          <SkillWidget techSkill={skill} />
+        ))}
+      </div>
     </div>
   );
 }
