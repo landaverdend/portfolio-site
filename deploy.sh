7a1a3601478e6e11872cf0b469147eb7b1cb1826
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2025-01-25 16:46:10 -0500

    script additions

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,19 @@
 # build the frontend package first.
 echo "Building frontend.."
 cd ./frontend/
+npm install
 npm run build
 
 cd ..
 cp -r ./frontend/dist/* ./backend/src/main/resources/static/
+
+cd ./backend/
+echo "Building backend"
+./gradlew clean build
+
+IMAGE_NAME="portfolio-site"
+
+echo "Building docker container"
+docker build --no-cache -t $IMAGE_NAME:latest .
+
+docker-compose up -d 
