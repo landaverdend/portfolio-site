f358f2d4dc22d943ab5a146e7941aa6d4eb37727
Nic <nicodemus.landaverde98@gmail.com>
2025-01-26 08:49:57 +0000

    asdfasdf

--- deploy.sh
+++ deploy.sh
@@ -17,5 +17,4 @@ IMAGE_NAME="portfolio-site"
 
 echo "Building docker container"
 docker build --no-cache -t $IMAGE_NAME:latest .
-docker-compose down
-docker-compose up -d 
+docker system prune -f 
