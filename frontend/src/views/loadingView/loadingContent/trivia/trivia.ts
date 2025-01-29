f1d95be622dbfe48b55c9573a654a4504652b10c
Nicodemus  Landaverde <nlandaverde@Nicodemuss-Air.lan>
2025-01-29 15:32:04 -0500

    some more trivia

--- frontend/src/views/loadingView/loadingContent/trivia/trivia.ts
+++ frontend/src/views/loadingView/loadingContent/trivia/trivia.ts
@@ -1,6 +1,5 @@
 const trivia = [
   'I am originally from Posadas, Argentina?',
-  'I am originally from Medellín, Colombia?',
   'I am originally from Wuhan, China?',
   'I am actually 6 feet tall?',
   'I have hyperhidrosis?',
@@ -8,7 +7,19 @@ const trivia = [
   'My favorite musical is Sweeney Todd?',
   'My social security number is: 625-74-5599?',
   'The 3 digits on the back of my Chase Sapphire Rewards Credit Card are: 725?',
-  '[[This trivia is only visible to Premium Users]]',
+  '[[This trivia is only visible to Pro Users]]',
+  'I am a bitcoin gazillionaire?',
+  'My hands get really dry in the winter?',
+  'I am a closeted unicyclist?',
+  'My name spelled backwards is edrevdnaL sumedociN',
+  'My debit card number is: 4218-2926-8645-7899 and the three numbers on the back are: 983',
+  'The password I use for my bank-account is: N1CkoD33MuS123!',
+  'I used to have asthma but I got rid of it through sheer will?',
+  'I have been good friends with William Alexander Sease since my freshman year of college?',
+  'My phone number is (919) 929-0246? Please call me!',
+  'I think that italian food is overrated?',
+  'I generally shower twice a day?',
+  'I can recite the alphabet backwards?',
   // Jokes below.
   'I am 2% West African?',
 ];
