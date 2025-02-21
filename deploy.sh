b10f8d3eef402e3af0a73dccfe14de17964f9e6b
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2025-02-20 23:25:47 -0500

    it's over.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # build the frontend package first.
+echo "Shutting systems down"
+docker-compose down
 echo "Building frontend.."
 cd ./frontend/
 npm install
@@ -17,4 +19,6 @@ IMAGE_NAME="portfolio-site"
 
 echo "Building docker container"
 docker build --no-cache -t $IMAGE_NAME:latest .
-docker system prune -f 
+docker system prune -f
+echo "spinning back up"
+docker-compose up -d 
