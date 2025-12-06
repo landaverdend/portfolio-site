59197ab985c769611ee122614a15a9d58be3b804
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-12-06 00:51:38 -0300

    messing around.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,9 @@ import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <nav className="w-3/4 mx-auto flex items-center justify-between bg-darkindigo/20 backdrop-blur-md shadow-lg text-white py-4 rounded-full mt-4">
+    <nav
+      className="w-2/3 mx-auto flex items-center justify-between text-white py-4 mt-4  bg-indigo-900 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border border-gray-100
+">
       <div className="w-1/3 text-center flex items-center justify-center gap-2">
         <Image src="/logo.png" alt="logo" width={50} height={50} className="rounded-full" />
         landaverde.io
