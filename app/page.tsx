6da921a1437dcb282ba125dc55a5feca763e7ebd
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-12-06 15:56:07 -0300

    mobile template

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+'use client';
+
+import { GlobeIcon } from '@radix-ui/react-icons';
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div className="">
-      <main className="w-full flex items-center justify-center ">Hello World</main>
+      <main className="w-full flex flex-col items-center justify-center gap-4 pt-10">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <Image src="/me.jpg" alt="logo" width={148} height={148} className="rounded-full border-1 border-gray-100 shadow-lg" />
+          <div className="flex flex-row items-center justify-center gap-2">
+            <GlobeIcon className="w-4 h-4" />
+            <span>Americas (EST)</span>
+          </div>
+          <div className="flex flex-row items-center justify-center gap-2">
+            <span className="text-white bg-indigo-400/10 rounded-lg px-2 py-1 text-xs border border-gray-400">English</span>
+            <span className="text-white bg-indigo-400/10 rounded-lg px-2 py-1 text-xs border border-gray-400">Spanish</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl font-extrabold tracking-tighter">Nico Landaverde</h1>
+          <span className="text-xl text-gray-500 font-light">Full Stack Developer</span>
+        </div>
+
+
+      </main>
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,9 +11,9 @@ export default function Navbar() {
       lg:top-5 lg:bottom-auto">
       <nav
         className="flex items-center justify-center text-center
-        lg:w-2/3 lg:justify-between 
-        py-4 px-4 bg-indigo-900 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border">
-        <div className="hidden text-center lg:w-1/3 lg:flex items-center justify-center gap-2">
+        lg:w-fit lg:justify-between 
+        py-2 lg:py-4 px-10 bg-indigo-900 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border">
+        <div className="hidden text-center lg:w-1/5 lg:flex items-center justify-center gap-2">
           <Image src="/logo.png" alt="logo" width={60} height={60} className="rounded-full" />
           <span className="font-semibold">landaverde.io</span>
         </div>
@@ -21,7 +21,7 @@ export default function Navbar() {
         <div className="">
           <ul className="flex items-center justify-between gap-5">
             <li>
-              <NavbarItem href="/about" icon={<PersonIcon />}>
+              <NavbarItem href="/" icon={<PersonIcon />}>
                 About
               </NavbarItem>
             </li>
@@ -49,7 +49,9 @@ export default function Navbar() {
 
 function NavbarItem({ href, icon, children }: { href: string; icon: React.ReactNode; children: React.ReactNode }) {
   return (
-    <Link href={href} className="flex flex-row gap-2 items-center justify-center transition-colors duration-300 hover:bg-indigo-100/10 rounded-full p-2">
+    <Link
+      href={href}
+      className="flex flex-row gap-2 items-center justify-center transition-colors duration-300 hover:bg-indigo-100/10 rounded-full py-2 px-3">
       {icon}
       <span className="hidden lg:block">{children}</span>
     </Link>
