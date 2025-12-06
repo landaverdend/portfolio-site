d36228547a6298e676a057e96d47b3572f4e1aea
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-12-05 23:04:44 -0300

    clearing out.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <div className="">
-      <main className="w-full flex items-center ">
-        Hello World
-      </main>
+      <main className="w-full flex items-center justify-center ">Hello World</main>
     </div>
   );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav className="w-screen flex items-center justify-between ">
+      <div className="w-1/3 text-center">landaverde.io</div>
+
+      <div className="">
+        <ul>
+          <li>Home</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
