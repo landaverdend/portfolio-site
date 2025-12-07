5fec5818748d59d5b48a89020200a619a229225b
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-12-07 15:56:57 -0300

    asdfasdf

--- components/ui/tech-carousel.tsx
+++ components/ui/tech-carousel.tsx
@@ -7,15 +7,15 @@ type TechCarouselProps = {
 export default function TechCarousel({}) {
   const items = [
     <ReactSVG className="fill-white w-10 h-10" />,
-    <SpringSVG className="fill-white w-8 h-8" />,
-    <TypescriptSVG className="w-8 h-8" />,
+    // <SpringSVG className="fill-white w-8 h-8" />,
+    // <TypescriptSVG className="w-8 h-8" />,
     <KafkaSVG className="fill-white text-white w-12 h-12" />,
     <AngularSVG className="fill-white w-9 h-9 text-white" />,
     <PostgreSQLSVG className="fill-white w-9 h-9 " />,
   ];
 
   return (
-    <div className="w-8/10 overflow-hidden sm:w-1/2 [mask-image:linear-gradient(90deg,transparent_0%,#000_10%,#000_90%,transparent_100%)]">
+    <div className="w-[325px] overflow-hidden mask-[linear-gradient(90deg,transparent_0%,#000_10%,#000_90%,transparent_100%)]">
       <div className="flex animate-dock-slide" style={{ width: '200%' }}>
         <div className="col-span-2 flex flex-row items-center gap-4" style={{ width: '50%', flexShrink: 0 }}>
           {items.map((item, i) => (
