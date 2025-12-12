1b9760e083b8deaaafd02d2761978ecb7f8fe2ff
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-12-12 16:32:08 -0300

    add umami script for analytics tracking...

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from '@/components/navbar';
 import BubbleBackground from '@/components/bubble-background';
 import ChatBubble from '@/components/chat-bubble';
 import { ChatProvider } from '@/contexts/chat-context';
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,6 +30,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased pb-[100px] lg:pt-[130px] text-white`}>
+        {process.env.NODE_ENV === 'production' && (
+          <Script defer src="https://umami.landaverde.in/script.js" data-website-id="17a3552e-fa37-4300-8d28-b2cdf08abaec" />
+        )}
+
         <ChatProvider>
           <BubbleBackground />
           <Navbar />
